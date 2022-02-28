e1e2d6d62fd144fe074d1f9e4333079ed0494658
Leedh585436 <63927332+Leedh585436@users.noreply.github.com>
2022-02-28 18:18:34 +0900

    1

---
+++ footer.php
@@ -0,0 +1,10 @@
+	<footer>
+		<nav class="navbar navbar-expand-sm bg-dark navbar-dark" role="navigation">
+			<div class="container">
+				<ul class="navbar-nav">
+					<li class="nav-item"><a class="nav-link" href="contact" title="Contact">Contact</a></li>
+					<li class="nav-item"><a class="nav-link" href="mentions-legales" title="Mentions">Mentions légales</a></li>
+				</ul>
+			</div>
+		</nav>
+	</footer>
